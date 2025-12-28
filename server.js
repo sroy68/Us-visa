895e32a14ae1d1d462590864571184724dfda5cd
sroy68 <sroy68@github.com>
2025-12-28 12:06:07 +0530

    Add root route + slots endpoint

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.json({ message: "US Visa Slot Tracker API is running!" });
+});
+
 app.get('/slots', (req, res) => {
   const demoSlots = [
     {"date":"2025-12-28","location":"Kolkata","time":"09:00","status":"Available"},
